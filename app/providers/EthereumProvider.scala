197a50cd2cca4533eadea1a0d9caf856f8c6f81c
adridadou <david@roon.me>
2016-07-26 18:45:37 +0200

    change in eth-contract-api

--- app/providers/EthereumProvider.scala
+++ app/providers/EthereumProvider.scala
@@ -1,12 +1,8 @@
 package providers
 
-import java.io.{File, FileInputStream}
-
-import com.google.common.base.Charsets
 import com.google.inject.Provider
 import org.adridadou.ethereum.EthereumFacade
 import org.adridadou.ethereum.provider.TestnetEthereumFacadeProvider
-import org.apache.commons.io.IOUtils
 
 /**
   * Created by davidroon on 24.07.16.
@@ -16,7 +12,6 @@ class EthereumProvider extends Provider[EthereumFacade] {
 
   override def get(): EthereumFacade = {
     val provider = new TestnetEthereumFacadeProvider()
-    val key = provider.getKey("cow","")
-    provider.create(key)
+    provider.create()
   }
 }
